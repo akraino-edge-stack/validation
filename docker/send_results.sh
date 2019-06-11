7eb8c46fea1a601a10b41c3943f7afa59879afc9
Naga Sugguna <ns156u@att.com>
2019-06-11 12:41:58 -0500

    Send Results to Nexus
    * this script sends blueprint results to nexus.
    * if runnning on master(snapshot) includes timestamp
    * creates the zip file and removes after pushing.
    * User responsible to keep required things in results dir.
    
    Change-Id: Iea09678318a8664338a86227588dd7100900ee5f
    Signed-off-by: Naga Sugguna <ns156u@att.com>

---
+++ docker/send_results.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# Copyright 2018 AT&T Intellectual Property.  All other rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e -u
+echo "==> send_results.sh"
+
+# Deploying logs to LFNexus log server ##
+
+NEXUS_URL=https://nexus.akraino.org
+SILO=att-blu-val
+BLUEPRNT=rec
+VERSION=master
+NEXUS_PATH="${SILO}/$BLUEPRNT/$VERSION"
+if [ "master" = $VERSION ]
+then
+  TIMESTAMP=$(date "+%Y%m%d-%H%M%S")
+  NEXUS_PATH="$NEXUS_PATH/$TIMESTAMP"
+fi
+
+zip -r results.zip results
+echo "executing lftools deploy nexus-zip $NEXUS_URL logs $NEXUS_PATH results.zip"
+lftools deploy nexus-zip $NEXUS_URL logs $NEXUS_PATH results.zip
+rm results.zip
