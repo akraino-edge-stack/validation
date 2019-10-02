akraino-edge-stack/validation

commit ffc875998692929c2e4c0881aec6b68c9f2fbab8

mysql: Skip build/push on aarch64
MySQL 5.6 is not officially available on aarch64 platforms, so
skip building/pushing the arm64 artifacts for our mysql container.

Change-Id: I21c6ae3cf33d46966e432c53f57cffc80d4d4170
Signed-off-by: Alexandru Avadanii <Alexandru.Avadanii@enea.com>

docker/build.mk
@@ -54,7 +54,7 @@ $(MTOOL):
 		.
 
 .PHONY: .push_image
-.push_image: .build
+.push_image:: .build
 	docker push $(REGISTRY)/$(NAME):$(TAG_PRE)-$(HOST_ARCH)-$(TAG_VER)
 
 .PHONY: .push_manifest

docker/mysql/Makefile
@@ -17,6 +17,18 @@
 .PHONY: all
 all: .push_image .push_manifest
 
+# MySQL 5.6 is not supported on arm64 yet, skip building it for now
+ifeq ($(shell uname -m), aarch64)
+.PHONY: .build
+.build::
+	echo "Skipping build on aarch64 platform!"
+	exit 0
+.PHONY: .push_image
+.push_image::
+	echo "Skipping push on aarch64 platform!"
+	exit 0
+endif
+
 .PHONY: build
 build: .build
 

commit 4021ce7f6addea03dc908a4d5989fe000a5b6448

docker: Fix aarch64 mysql docker build/push skip
The previous attempt of skipping the mysql container image build on
aarch64 did not really work.

Fixes: ffc875998692929c2e4c0881aec6b68c9f2fbab8

Change-Id: I1fe2d0ad05fda224db1991d07f46653f89b5a82b
Signed-off-by: Alexandru Avadanii <Alexandru.Avadanii@enea.com>

docker/build.mk
@@ -54,7 +54,7 @@ $(MTOOL):
 		.
 
 .PHONY: .push_image
-.push_image:: .build
+.push_image: .build
 	docker push $(REGISTRY)/$(NAME):$(TAG_PRE)-$(HOST_ARCH)-$(TAG_VER)
 
 .PHONY: .push_manifest

docker/mysql/Makefile
@@ -18,18 +18,17 @@
 all: .push_image .push_manifest
 
 # MySQL 5.6 is not supported on arm64 yet, skip building it for now
-ifeq ($(shell uname -m), aarch64)
-.PHONY: .build
-.build::
+.PHONY: build
+ifneq ($(shell uname -m), aarch64)
+build: .build
+else
+build:
 	echo "Skipping build on aarch64 platform!"
 	exit 0
 .PHONY: .push_image
-.push_image::
+.push_image:
 	echo "Skipping push on aarch64 platform!"
 	exit 0
 endif
-
-.PHONY: build
-build: .build
 
 include ../build.mk