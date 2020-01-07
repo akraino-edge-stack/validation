b8b09609720c4fc2ba5da1766bf13e0f9944290a
Daniel Stoica <daniel.stoica@enea.com>
2020-01-07 13:33:53 +0200

    WIP: Add Vuls security test for x86_64
    Signed-off-by: Daniel Stoica <daniel.stoica@enea.com>
    Change-Id: I6aa553d50fc33327ee893382aec0eee605a94eb1

---
+++ tests/os/vuls/environment.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# Install depend
+apk update
+apk add make gcc libc-dev sqlite
+
+# Install go
+wget https://dl.google.com/go/go1.12.6.linux-amd64.tar.gz -P /root
+cd /root/ || exit
+tar -xzf go1.12.6.linux-amd64.tar.gz
+
+# Set go path
+echo "export GOROOOT=/root/go" >> /etc/profile
+echo "export GOPATH=/root/go/src" >> /etc/profile
+echo "export PATH=$PATH:/root/go/bin:/root/go/src/bin" >> /etc/profile
+. /etc/profile
+mkdir /lib64 && ln -s /lib/libc.musl-x86_64.so.1 /lib64/ld-linux-x86-64.so.2
+
+# Add go-cve-dictionary
+cd /root/go/src || exit
+mkdir -p /root/go/src/github.com/kotakanbe
+git -C /root/go/src/github.com/kotakanbe clone https://github.com/kotakanbe/go-cve-dictionary.git
+make -C /root/go/src/github.com/kotakanbe/go-cve-dictionary install
+for i in $(seq 2002 "$(date +"%Y")"); do go-cve-dictionary fetchnvd -dbpath /vuls/cve.sqlite3 -years "$i"; done
+
+# Add goval-dictionary
+git -C /root/go/src/github.com/kotakanbe clone https://github.com/kotakanbe/goval-dictionary.git
+make -C /root/go/src/github.com/kotakanbe/goval-dictionary install
+goval-dictionary fetch-ubuntu -dbpath=/vuls/oval.sqlite3 18
+
+# Set ssh configuration
+touch /root/.ssh/config
+echo "Host *" > ~/.ssh/config
+echo "StrictHostKeyChecking no" >> ~/.ssh/config
+
+# Run vuls test
+cd /vuls || exit
+vuls scan -config /vuls/config.toml -ssh-config
+touch vuls.log
+vuls report | tee -a /vuls/vuls.log
+
